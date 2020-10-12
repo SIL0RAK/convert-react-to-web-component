6a57df3765817ba7c32b98bec78e701936c978c8
sil0rak <karolis.maluno15@gmail.com>
2020-10-12 21:20:29 +0300

    update demo and readme

--- demo/PersonScore.jsx
+++ demo/PersonScore.jsx
@@ -1,10 +1,19 @@
-import React, { memo } from 'react';
+import React, { memo, useState } from 'react';
 
-const PersonScore = ({ name = 'petras', score = 10 }) => (
-    <div>
-        <h1>{name}</h1>
-        <h5>{score}</h5>
-    </div>
-);
+const PersonScore = ({ name = 'petras', score = 10 }) => {
+    const [checked, setChecked] = useState(false)
+
+    return (
+        <div className="person-score">    
+            <h1>{name}</h1>
+            <h5>{score}</h5>
+            <input
+                type="checkbox"
+                checked={checked}
+                onClick={() => setChecked(!checked)}
+            />
+        </div>
+    )
+};
 
 export default memo(PersonScore);
