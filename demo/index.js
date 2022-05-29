fbdbaaab5df620f3ff38ca9bdc710da29595fd0b
sil0rak <karolis.maluno15@gmail.com>
2022-05-29 18:40:27 +0300

    feat: shadow mode

--- demo/index.js
+++ demo/index.js
@@ -5,9 +5,10 @@ import './angular-app.js';
 const middleware = (value) => value.includes('{{') ? undefined : value;
 
 const options = {
-    attributes: ['score', 'name'], // list of attributes to listen for.
+    attributes: ['score', 'person-name'], // list of attributes to listen for. Notice that attribute will be converted to camelCase
     name: 'person-score',  // optional parameter to rename component if not provided will use react component name.
-    middleware // middleware for parsing attributes. Usefull with frameworks that use templates.
+    middleware, // middleware for parsing attributes. Usefull with frameworks that use templates.
+    useShadowDom: true
 };
 
 create(PersonScore, options);
--- src/create.ts
+++ src/create.ts
@@ -8,59 +8,66 @@ interface IOptions {
     attributes?: Array<string>;
     name?: string;
     middleware: (prop: string) => string | unknown;
-    useShadowDom: boolean;
+    shadowDom?: ShadowRootMode;
 }
 
-const create = (
+/** generates webComponent from provided react component and options */
+const webComponentFactory = (
     Component: React.FunctionComponent,
     options?: IOptions
-) => {
-    class WebComponent extends HTMLElement {
-        private props: Record<string, unknown>;
+) => class WebComponent extends HTMLElement {
+    private props: Record<string, unknown>;
 
-        constructor () {
-            super();
+    constructor () {
+        super();
 
-            this.props = {};
-        }
-
-        static get observedAttributes() {
-            return options?.attributes ? getAsSnakeCase(options?.attributes) : [];
-        }
-
-        connectedCallback() {
-            this.render();
-        }
-
-        disconnectedCallback() {
-            unmountComponentAtNode(this);
-        }
-
-        attributeChangedCallback(name, _oldValue, newValue) {
-            this.props[getAsPascalCase(name)] = options?.middleware(newValue) || newValue;
-            this.render();
-        }
-
-        render() {
-            if (this.isConnected) {
-                // if useShadowDom enabled create shadow root and use it as mountPoint for react app
-                let mountPoint = options.useShadowDom
-                    ? this.attachShadow({mode: 'open'})
-                    : this;
-                
-                render(
-                    createElement(Component, {...this.props}, null),
-                    mountPoint,
-                );
-            }
+        this.props = {};
+        // if useShadowDom enabled create shadow root and use it as mountPoint for react app
+        if (options.shadowDom) {
+            this.attachShadow({mode: options.shadowDom });
         }
     }
+
+    static get observedAttributes() {
+        return options?.attributes ? options?.attributes.map(getAsSnakeCase) : [];
+    }
+
+    connectedCallback() {
+        this.render();
+    }
+
+    disconnectedCallback() {
+        unmountComponentAtNode(this);
+    }
+
+    attributeChangedCallback(name, _oldValue, newValue) {
+        this.props[getAsPascalCase(name)] = options?.middleware(newValue) || newValue;
+        this.render();
+    }
+
+    private render() {
+        if (this.isConnected) {
+            render(
+                createElement(Component, {...this.props}, null),
+                options.shadowDom ? this.shadowRoot: this,
+            );
+        }
+    }
+}
+
+const addWebComponentToCustomElements = (WebComponent: CustomElementConstructor, name: string) => {
+    if (customElements.get(name) === undefined) {
+        customElements.define(name, WebComponent);
+    }
+};
+
+/** generates webComponent and adds it to custom elements list. */
+const create = (Component: React.FunctionComponent, options?: IOptions) => {
+    const WebComponent = webComponentFactory(Component, options);
 
     const webComponentName =  options?.name || getAsSnakeCase(Component.name);
 
-    if (customElements.get(webComponentName) === undefined) {
-        customElements.define(webComponentName, WebComponent);
-    }
+    addWebComponentToCustomElements(WebComponent, webComponentName);
 };
 
 export default create;
